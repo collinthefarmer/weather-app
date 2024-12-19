a3fb8adda6ac96a4ce50d1f82b272da0e699fcd9
Collin Farmer <collinthefarmer@gmail.com>
2024-12-18 22:04:52 -0500

    paintingggg

--- internal/weather/weather.go
+++ internal/weather/weather.go
@@ -1,9 +1,23 @@
 package weather
 
+import "math"
+
+const openMeteoPrecision = 2
+
+// https://stackoverflow.com/a/29786394
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
 func FToC(f float64) float64 {
-	return f - 32.0*(5.0/9.0)
+	return toFixed(f-32.0*(5.0/9.0), openMeteoPrecision)
 }
 
 func CToF(c float64) float64 {
-	return (9.0/5.0)*c + 32.0
+	return toFixed((9.0/5.0)*c+32.0, openMeteoPrecision)
 }
--- static/js/components.js
+++ static/js/components.js
@@ -11,7 +11,116 @@ function initObservationCanvas() {
         constructor() {
             super();
         }
+
+        connectedCallback() {
+            this.style = "position: relative";
+
+            const shadow = this.attachShadow({ mode: "open" });
+            const canvas = document.createElement("canvas");
+            canvas.style = "position: absolute; inset: 0 0 0 0;";
+            canvas.width = 500;
+            canvas.height = 500;
+            shadow.appendChild(canvas);
+
+            this.canvas = canvas;
+            this.cursor = new ObservationCanvasBrush(canvas, 10);
+
+            canvas.addEventListener("mousedown", () => {
+                this.cursor.start();
+            });
+
+            canvas.addEventListener("mousemove", (ev) => {
+                this.cursor.paint(
+                    ev.clientX - this.offsetLeft,
+                    ev.clientY - this.offsetTop,
+                );
+            });
+
+            canvas.addEventListener("mouseup", () => {
+                this.cursor.stop();
+            });
+        }
+    }
+
+    class ObservationCanvasBrush {
+        constructor(canvas, size, defaultColor = "#000000") {
+            this.canvas = canvas;
+            this.context = canvas.getContext("2d");
+
+            const paintBufferSize = 255;
+            this.paintBuffer = new Uint16Array(paintBufferSize);
+            this.paintBufferPos = 0;
+
+            this.painting = false;
+
+            this.size = size;
+            this.color = defaultColor;
+        }
+
+        start() {
+            this.painting = true;
+            this.context.beginPath();
+        }
+
+        paint(x, y) {
+            if (this.painting) {
+                this.context.ellipse(
+                    x,
+                    y,
+                    this.size,
+                    this.size,
+                    0,
+                    0,
+                    2 * Math.PI,
+                );
+                this.context.fillStyle = this.color;
+                this.context.fill();
+            }
+            this.context.moveTo(x, y);
+        }
+
+        stop() {
+            this.painting = false;
+        }
+
+        applyPaintBuffer() {
+            this.context.ellipse(x, y, this.size, this.size, 0, 0, 2 * Math.PI);
+            this.context.fill();
+        }
+    }
+
+    class ObservationCanvasPallete extends HTMLElement {
+        static observedAttributes = ["for"];
+
+        constructor() {
+            super();
+        }
+
+        attributeChangedCallback(attr, oldval, newval) {
+            switch (attr) {
+                case "for":
+                    if (oldval) {
+                        this.disconnectCanvas(oldval);
+                    }
+                    this.connectCanvas(newval);
+                    break;
+            }
+        }
+
+        disconnectCanvas(id) {
+            const canvas = document.querySelector("#" + id);
+            console.log(canvas);
+        }
+
+        connectCanvas(id) {
+            const canvas = document.querySelector("#" + id);
+            console.log(canvas);
+        }
     }
 
     customElementRegistry.define("observation-canvas", ObservationCanvas);
+    customElementRegistry.define(
+        "observation-canvas-pallete",
+        ObservationCanvasPallete,
+    );
 }
